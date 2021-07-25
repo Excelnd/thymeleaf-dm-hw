4c6521df2949bb3ee758afe4866af1d4377542cc
Admin i <ihsanmpm@gmail.com>
2021-07-25 20:35:40 +0530

    init,

---
+++ src/main/java/com/ihs2code/springboot/thymeleafdm/ThymeleafdmApplication.java
@@ -0,0 +1,13 @@
+package com.ihs2code.springboot.thymeleafdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ihs2code/springboot/thymeleafdm/ThymeleafdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihs2code.springboot.thymeleafdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
