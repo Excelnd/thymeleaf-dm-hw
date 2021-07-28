b5d887a2cce854324659a100c969aeccf2ccf026
Admin i <ihsanmpm@gmail.com>
2021-07-28 10:49:55 +0530

    EmployeeRepository findAllByOrderByLastNameAsc

--- src/main/java/com/ihs2code/springboot/thymeleafdm/dao/EmployeeRepository.java
+++ src/main/java/com/ihs2code/springboot/thymeleafdm/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.ihs2code.springboot.thymeleafdm.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ihs2code.springboot.thymeleafdm.entity.Employee;
@@ -7,4 +9,7 @@ import com.ihs2code.springboot.thymeleafdm.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// no need to write additional code for CRUD
+	
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
